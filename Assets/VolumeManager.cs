8287a40926e72f366c22b51bb3aea33047d75918
Matthew Middlebrook <mkm15a@acu.edu>
2019-11-08 15:36:38 -0600

    Added basic UI

---
+++ Assets/VolumeManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class VolumeManager : MonoBehaviour
+{
+    public AudioMixer masterMixer;
+
+    public Slider musicSlider;
+    public string musicParamater;
+
+    public Slider UISlider;
+    public string UIParamater;
+
+    void OnEnable()
+    {
+        float value;
+        if (masterMixer.GetFloat(musicParamater, out value))
+		{
+			musicSlider.value = Mathf.Pow(10, value/20);
+		}
+        if (masterMixer.GetFloat(UIParamater, out value))
+		{
+			UISlider.value = Mathf.Pow(10, value/20);
+		}
+    }
+
+    public void SetMusicLevel(float level)
+	{
+		masterMixer.SetFloat(musicParamater, Mathf.Log10(level) * 20);
+	}
+
+    public void SetUILevel(float level)
+	{
+		masterMixer.SetFloat(UIParamater, Mathf.Log10(level) * 20);
+	}
+}
