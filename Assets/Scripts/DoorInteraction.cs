096912bcdb35b30e6b0267f9fa0bc6dd3f71cc41
Matthew Middlebrook <mkm15a@acu.edu>
2019-11-11 11:58:09 -0600

    Added cubes around doors that are for cats

---
+++ Assets/Scripts/DoorInteraction.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorInteraction : MonoBehaviour
+{
+    public float openSpeed = 4;
+    public float openAngle = 105;
+    
+    [SerializeField]
+    private bool isOpen = false;
+    private Quaternion startingRotation;
+    private Quaternion endingRotation;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingRotation = transform.localRotation;
+        Vector3 rot = startingRotation.eulerAngles;
+        rot = new Vector3(rot.x, rot.y - openAngle, rot.z);
+        endingRotation = Quaternion.Euler(rot);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isOpen)
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, endingRotation, Time.deltaTime * openSpeed);
+        else
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, startingRotation, Time.deltaTime * openSpeed);
+    }
+
+    public void ToggleDoor()
+    {
+        isOpen = !isOpen;
+    }
+}
