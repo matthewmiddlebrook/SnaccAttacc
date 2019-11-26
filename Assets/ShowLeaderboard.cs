e36fbb02f875c5a5cc5cc0fec242e140377b1537
Matthew Middlebrook <mkm15a@acu.edu>
2019-11-26 10:34:41 -0600

    Working on Leaderboard integration

---
+++ Assets/ShowLeaderboard.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GooglePlayGames;
+using UnityEngine.SocialPlatforms;
+
+public class ShowLeaderboard : MonoBehaviour
+{
+    public void Show() {
+        // show leaderboard UI
+        Social.ShowLeaderboardUI();
+    }
+}
