7189d591ddd68880c2edcd44895018aa00c9e246
Matthew Middlebrook <mkm15a@acu.edu>
2020-02-02 17:41:00 -0600

    Replaced and removed DoozyUI since it makes exporting and opening the project take longer

---
+++ Assets/Scripts/LoadingScreen.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadingScreen : MonoBehaviour
+{
+    public Text loadingText;
+    public string loadingScene;
+
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        StartCoroutine(LoadScene());
+    }
+
+    IEnumerator LoadScene()
+    {
+        yield return null;
+
+        //Begin to load the Scene you specify
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(loadingScene);
+        //Don't let the Scene activate until you allow it to
+        asyncOperation.allowSceneActivation = false;
+        Debug.Log("Pro :" + asyncOperation.progress);
+        //When the load is still in progress, output the Text and progress bar
+        while (!asyncOperation.isDone)
+        {
+            //Output the current progress
+            loadingText.text = (int)(asyncOperation.progress * 100) + "%";
+
+            // Check if the load has finished
+            if (asyncOperation.progress >= 0.9f)
+            {
+                asyncOperation.allowSceneActivation = true;
+            }
+
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/PageManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PageManager : MonoBehaviour
+{
+    public GameObject[] pages;
+    public GameObject startPage;
+    GameObject currentPage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentPage = startPage;
+        foreach (GameObject p in pages) 
+        {
+            if (p == startPage)
+                p.SetActive(true);
+            else
+                p.SetActive(false);
+        }
+    }
+
+    public void SwitchPage(GameObject newPage)
+    {
+        currentPage.SetActive(false);
+        newPage.SetActive(true);
+        currentPage = newPage;
+    }
+}
